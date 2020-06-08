04430d85be2d69105b1d0560457fa027c8b1f424
Jen Chow <jennego@gmail.com>
2020-06-07 23:41:41 -0700

    it didn't work

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import Coats from "./coats"
+import Coats from "../components/coats"
 
 const IndexPage = () => (
   <Layout>
